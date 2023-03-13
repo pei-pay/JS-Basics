f1be1ce90ada16eeda0f294b20f28de184162535
pei-pay <gleektennis@icloud.com>
2023-03-13 23:55:15 +0900

    update: this

--- this/index.js
+++ this/index.js
@@ -1,7 +1,18 @@
+window.name = 'John'
 const person = {
   name: 'Tom',
   hello: function () {
     console.log('Hello ' + this.name)
   }
 }
+/*
+ * オブジェクトのメソッドとして実行される場合
+ * `this` => 呼び出し元オブジェクト
+ */
 person.hello()
+/*
+ * 関数として実行される場合
+ * `this` => グローバルオブジェクト
+ */
+const ref = person.hello
+ref()
